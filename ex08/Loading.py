9cd33d51d53b7dc5b6dfcb14bb1a880f6ba15817
justincapik <justincapik@gmail.com>
2024-02-01 15:42:58 +0100

    norm

--- ex08/Loading.py
+++ ex08/Loading.py
@@ -2,14 +2,15 @@ def ft_tqdm(lst: range) -> None:
 
     width = 87
 
-    for l in range(len(lst)):
-        l += 1
-        percentage = int(l/len(lst) * 100)
+    for it in range(len(lst)):
+        it += 1
+        percentage = int(it/len(lst) * 100)
 
         whitecount = int(width * percentage / 100)
 
         progressbar = "█" * min(whitecount, width) + " " * (width - whitecount)
 
-        print ("{:3d}%|".format(percentage) + progressbar + "| {:d}/{:d}".format(l, len(lst)), end="\r")
-        
+        print("{:3d}%|".format(percentage) +
+              progressbar + "| {:d}/{:d}".format(it, len(lst)), end="\r")
+
         yield
